4ed49f4ea811972392b3f9fc1b40a95d9772c156
Daniil Galitskii <daniil.g@byallmeans.work>
2024-01-17 16:59:08 +0100

    feat/ended crud with db

--- 3-crud-db/pkg/controller/book-controller.go
+++ 3-crud-db/pkg/controller/book-controller.go
@@ -1,5 +1,120 @@
 package controller
 
-func CreateBook() {
+import (
+	"crud-db/pkg/model"
+	"crud-db/pkg/util"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
+)
+
+var NewBook model.Book
+
+func GetBooks(w http.ResponseWriter, r *http.Request) {
+	newBooks := model.GetAllBooks()
+
+	res, _ := json.Marshal(newBooks)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+
+	bookDetails, _ := model.GetBookById(ID)
+	res, _ := json.Marshal(bookDetails)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	CreateBook := &model.Book{}
+	util.ParseBody(r, CreateBook)
+	b, err := CreateBook.CreateBook()
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		res, _ := json.Marshal(err)
+		w.Write(res)
+	}
+
+	res, _ := json.Marshal(b)
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write(res)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+
+	book, deleteErr := model.DeleteBook(ID)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if deleteErr != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		res, _ := json.Marshal(err)
+		w.Write(res)
+	}
+
+	res, _ := json.Marshal(book)
+
+	w.WriteHeader(http.StatusAccepted)
+	w.Write(res)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	var updateBook = &model.Book{}
+	util.ParseBody(r, updateBook)
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+
+	bookDetails, db := model.GetBookById(ID)
+
+	if updateBook.Name != "" {
+		bookDetails.Name = updateBook.Name
+	}
+
+	if updateBook.Author != "" {
+		bookDetails.Author = updateBook.Author
+	}
+
+	if updateBook.Publication != "" {
+		bookDetails.Publication = updateBook.Publication
+	}
+
+	db.Save(&bookDetails)
+
+	res, _ := json.Marshal(bookDetails)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+	w.Write(res)
 }
--- 3-crud-db/pkg/router/bookstore-router.go
+++ 3-crud-db/pkg/router/bookstore-router.go
@@ -9,7 +9,7 @@ import (
 var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/book", controller.CreateBook).Methods("POST")
 	router.HandleFunc("/book", controller.GetBooks).Methods("GET")
-	router.HandleFunc("/book/{id}", controller.GetBookById).Methods("GET")
-	router.HandleFunc("/book/{id}", controller.DeleteBook).Methods("DELETE")
-	router.HandleFunc("/book/{id}", controller.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controller.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controller.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/book/{bookId}", controller.UpdateBook).Methods("PUT")
 }
